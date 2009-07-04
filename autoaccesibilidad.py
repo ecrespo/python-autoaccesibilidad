7f161ef7a29dc13b42e0d49d0e4822ab3a9424c5
ernesto <ernesto@zvezda>
2009-07-04 14:40:29 -0530

    agregado el programa principal autoaccesibilidad.py

---
+++ autoaccesibilidad.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Name: autoaccesibilidad
+Description: Programa principal que permite instalar aplicaciones accesibles de un repositorio local
+Version:0.1
+License: GPLv3
+Copyright: Copyright (C) 2009 Proyecto Libre Accesibilidad - Distrito Socialista Tecnologico AIT PDVSA  <moderador@libreaccesibilidad.org>
+Author: Ernesto Nadir Crespo Avila
+Email: ernesto@libreaccesibilidad.org
+"""
