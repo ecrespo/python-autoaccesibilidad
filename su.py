c6cf21fdc3f756c2d8952becb63742f2b5791bfb
ernesto <ernesto@zvezda>
2009-07-04 08:45:07 -0530

    agregado el modulo su.py

---
+++ su.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Name: ejecutar-root
+Description: Modulo que permite la ejecución de comandos como root
+Version:0.1
+License: GPLv3
+Copyright: Copyright (C) 2009  Ernesto Nadir Crespo Avila <ecrespo@debianvenezuela.org>
+Author: Ernesto Nadir Crespo Avila
+Email: ecrespo@debianvenezuela.org
+"""
+
+
+
+import gksu2 as gksu
+import gobject
+import getpass
+
+def ask_pass_func(context, prompt):
+    try:
+        return getpass.getpass("Enter root password: ")
+    except KeyboardInterrupt:
+        print
+        err = gobject.GError("keyboard interrupt")
+        err.code = gksu.ERROR_CANCELED
+        err.domain = "libgksu"
+        raise err
+
+def ejecutar(comando):
+    ctx = gksu.Context()
+    ctx.set_command(comando)
+    gksu.su_full(ctx, ask_pass=ask_pass_func)
