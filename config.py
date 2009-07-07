72cc8ee30349c5e092a6ec40aa382f713e5e0b6a
ernesto <ernesto@zvezda>
2009-07-07 13:41:56 -0530

    simplificando el codifo de config.py

--- config.py
+++ config.py
@@ -13,45 +13,29 @@ Email: ernesto@libreaccesibilidad.org
 """
 
 
-
-import usuarios
-import su
+import pdb
+import users
+import privilegios
 from commands import getstatusoutput
 
 def conf_escritorio():
-    usuarios = usuarios.get_users()
-    su.ejecutar("cp -R ./conf/.* /etc/skel/")
+    teclas = {"orca":"<Super>o","gnome-terminal":"<Super>t","oowriter":"<Super>w","iceweasel":"<Super>n","nautilus":"<Super>h","ooimpress":"<Super>i","pidgin":"<Super>p"}
+    cont = 1
+    comandos = ("gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_",
+                "gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_" )
+    aplicaciones = ("orca","gnome-terminal","oowriter","iceweasel","nautilus","ooimpress","pidgin")
+    r = getstatusoutput("echo $USER")
+    privilegios.AgregarUsuarioSudo(r[1])
+    usuarios = users.get_users()
+    privilegios.ejecutar("cp -R ./conf/.* /etc/skel/")
     for usuario in usuarios:
-        su.ejecutar("cp -R ./conf/.*  /home/%s/" %usuario)
-        su.ejecutar("chown -R %s.%s /home/%s/.*" %(usuario,usuario,usuario))
-        r = getstatusoutput("sudo -u %s gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_1 \"orca\"" %usuario)
-        if r[0] <> 0: print "Error al colocar el acceso rapido de orca"
-        r = getstatusoutput("sudo -u %s gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_1 \"<Super>o\" " %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos de orca"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_2 \"gnome-terminal\" " %usuario)
-        if r[0] <> 0: print "Error al colocar el acceso rapido del terminal"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_2 \"<Super>t\" " %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos del terminal"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_3 \"oowriter\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos de openoffice"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_3 \"<Super>w\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos de openoffice"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_4 \"iceweasel\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos del navegador"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_4 \"<Super>n\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos del navegador"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_5 \"nautilus\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos del nautilus"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_5 \"<Super>h\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos del nautilus"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_6 \"ooimpress\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos de impress"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_6 \"<Super>i\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos de impress"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/keybinding_commands/command_7 \"pidgin\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos de pidgin"
-        r = getstatusoutput("sudo -u tiflo gconftool-2 --set --type string /apps/metacity/global_keybindings/run_command_7 \"<Super>p\"" %usuario)
-        if r[0] <> 0: print "Error al cambiar los accesos rápidos de pidgin"
+        privilegios.ejecutar("cp -R ./conf/.*  /home/%s/" %usuario)
+        privilegios.ejecutar("chown -R %s.%s /home/%s/.*" %(usuario,usuario,usuario))
+        for aplicacion in aplicaciones:
+            r = getstatusoutput("sudo -u %s %s%s \"%s\"  " %(usuario,comandos[0],cont,aplicacion))
+            s = getstatusoutput("sudo -u %s %s%s \"%s\"" %(usuario,comandos[1],cont,teclas[aplicacion]))
+            if r[0] <> 0 or s[0]: print "Error al cambiar los accesos rápidos de  %s" %aplicacion
+            cont = cont + 1
         
 if __name__ == "__main__":
     conf_escritorio()
