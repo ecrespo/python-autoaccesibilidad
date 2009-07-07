62aed1eb39540e3ecf5f5f3a0a1348531623e553
ernesto <ernesto@zvezda>
2009-07-07 13:43:53 -0530

    agregado modulos a autoaccesibilidad

--- autoaccesibilidad.py
+++ autoaccesibilidad.py
@@ -10,3 +10,26 @@ Copyright: Copyright (C) 2009 Proyecto Libre Accesibilidad - Distrito Socialista
 Author: Ernesto Nadir Crespo Avila
 Email: ernesto@libreaccesibilidad.org
 """
+import pdb
+import config
+import aptitude
+import privilegios
+import users 
+import sincronizar
+from commands import getstatusoutput
+
+def Inicio():
+    print "Actualiza el repositorio local con respecto al repositorio principal"
+    sincronizar.actualizar
+    print "Inicia el servidor web"
+    print getstatusoutput("./web.py &")
+    print "Instala aplicaciones necesarias para darle accesibilidad al escritorio"
+    aptitude.apt.main("accesibilidad")
+    print "Configuración de los escritorios de los usuarios"
+    config.conf_escritorio
+    
+    
+if __name__ == "__main__":
+    Inicio()
+    
+    
